8491735abe1c021b72da08a6fae60bb4eb984319
ShenHang <54196007+byshenhang@users.noreply.github.com>
2024-11-14 23:20:33 +0800

    屏蔽版本验证

--- src/hooks/useVersionCheck.js
+++ src/hooks/useVersionCheck.js
@@ -1,9 +1,19 @@
+/*
+ * @Author: ShenHang 54196007+byshenhang@users.noreply.github.com
+ * @Date: 2024-11-14 23:14:32
+ * @LastEditors: ShenHang 54196007+byshenhang@users.noreply.github.com
+ * @LastEditTime: 2024-11-14 23:17:51
+ * @FilePath: \ReactFlux\src\hooks\useVersionCheck.js
+ * @Description:
+ *
+ * Copyright (c) 2024 by ${git_name_email}, All Rights Reserved.
+ */
 import { ofetch } from "ofetch"
 import { useEffect, useState } from "react"
 
 import { GITHUB_REPO_PATH, UPDATE_NOTIFICATION_KEY } from "@/utils/constants"
 import { checkIsInLast24Hours, getTimestamp } from "@/utils/date"
-import buildInfo from "@/version-info.json"
+// import buildInfo from "@/version-info.json"
 
 function useVersionCheck() {
   const [hasUpdate, setHasUpdate] = useState(false)
@@ -27,10 +37,10 @@ function useVersionCheck() {
 
         const data = await ofetch(`https://api.github.com/repos/${GITHUB_REPO_PATH}/commits/main`)
 
-        const currentGitTimestamp = getTimestamp(buildInfo.gitDate)
-        const latestGitTimestamp = getTimestamp(data.commit.committer.date)
+        // const currentGitTimestamp = getTimestamp(buildInfo.gitDate)
+        // const latestGitTimestamp = getTimestamp(data.commit.committer.date)
 
-        setHasUpdate(currentGitTimestamp < latestGitTimestamp)
+        // setHasUpdate(currentGitTimestamp < latestGitTimestamp)
       } catch (error) {
         console.error("Check update failed", error)
       }
